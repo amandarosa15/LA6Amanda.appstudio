79ebf3bf5a0766af3071fa1e71219717ccb494ac
amandarosa15 <79384892+amandarosa15@users.noreply.github.com>
2021-03-09 17:05:15 -0600

    Create of different Forms and first requierement

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,14 @@
-/*
-let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
 
+let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+
+
+Verify.onclick=function(){
+  let InputName = name.value
+  let check= members.includes(InputName)
+  if (check == true)
+    lblMessage.textContent = "You are already a member!"
+  if (check == false) {
+    members.push(InputName)
+    lblMessage.textContent = "You were not a member :(, but you are now!"
+    }
+}
